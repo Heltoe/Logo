ed5c7545d8abed8a36cd05a6b1e22aca1de3fee6
Heltoe <x-outhink@mail.ru>
2018-04-15 17:34:00 +0300

    Анимированный логотип

---
+++ logo.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/logo.css">
+    <title></title>
+  </head>
+  <body>
+    <div class="box">
+  		<div class="logo">
+  			<div class="logo-border"></div>
+  			<div class="logo-circle"></div>
+  			<div class="logo-dot"></div>
+  		</div>
+  	</div>
+  </body>
+</html>
